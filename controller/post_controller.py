37482665eee385a32ebe49366aacacc73d482c0a
Aniket Kalra <21f3002102@student.onlinedegree.iitm.ac.in>
2022-12-19 00:07:03 +0530

    Post logic impletemeted Please prefer previous commit for details

---
+++ controller/post_controller.py
@@ -0,0 +1,7 @@
+from model.post_model_controller import PostModelManager
+
+p_m_m = PostModelManager()
+print('creating post model')
+'''
+Post Mostly edited via api so most of the stuff will exists in Api folder
+'''
